cae365c71ff36a40a8a72e16c335c24f22f72479
characterdog <43253617+characterdog@users.noreply.github.com>
2018-10-28 16:44:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Share my Number
+
+Share your contact information with ease
+
+<img src="fastlane/metadata/android/en-US/images/icon.png" width="150px">
+
+<img src="fastlane/metadata/android/en-US/images/phoneScreenshots/1.png" width="400px"> <img src="fastlane/metadata/android/en-US/images/phoneScreenshots/2.png" width="400px"> <img src="fastlane/metadata/android/en-US/images/phoneScreenshots/3.png" width="400px">
+
+## License
+
+Published under [GNU General Public License v3.0 or later](https://spdx.org/licenses/GPL-3.0-or-later.html).
---
+++ app/src/main/java/com/github/characterdog/share_my_number/PreferenceActivity.java
@@ -0,0 +1,32 @@
+package com.github.characterdog.share_my_number;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+
+public class PreferenceActivity extends AppCompatPreferenceActivity {
+    public final static String PREF_PRIVATE_NAME = "name_private";
+    public final static String PREF_PRIVATE_EMAIL = "email_private";
+    public final static String PREF_PRIVATE_ADDRESS = "address_private";
+    public final static String PREF_PRIVATE_TITLE = "title_private";
+    public final static String PREF_PRIVATE_COMPANY = "company_private";
+    public final static String PREF_PRIVATE_PHONE = "phoneNumber_private";
+    public final static String PREF_PRIVATE_WEBSITE = "website_private";
+
+    public final static String PREF_COMPANY_NAME = "name_company";
+    public final static String PREF_COMPANY_EMAIL = "email_company";
+    public final static String PREF_COMPANY_ADDRESS = "address_company";
+    public final static String PREF_COMPANY_TITLE = "title_company";
+    public final static String PREF_COMPANY_COMPANY = "company_company";
+    public final static String PREF_COMPANY_PHONE = "phoneNumber_company";
+    public final static String PREF_COMPANY_WEBSITE = "website_company";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
