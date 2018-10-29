5879ad6d5bf9be995ff2b8a22a09c1d79ee23df3
characterdog <43253617+characterdog@users.noreply.github.com>
2018-10-29 19:11:25 +0100

    Add screenshot prepare 'test'

---
+++ app/src/androidTest/java/com/github/characterdog/share_my_number/SetupAppForScreenshots.java
@@ -0,0 +1,62 @@
+package com.github.characterdog.share_my_number;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static com.github.characterdog.share_my_number.PreferenceActivity.*;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SetupAppForScreenshots {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void initSettings() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        sharedPreferences.edit()
+                .putString(PREF_PRIVATE_NAME, context.getString(R.string.screenshots_private_name))
+                .putString(PREF_PRIVATE_EMAIL, context.getString(R.string.screenshots_private_email))
+                .putString(PREF_PRIVATE_ADDRESS, context.getString(R.string.screenshots_private_address))
+                .putString(PREF_PRIVATE_TITLE, context.getString(R.string.screenshots_private_title))
+                .putString(PREF_PRIVATE_COMPANY, context.getString(R.string.screenshots_private_company))
+                .putString(PREF_PRIVATE_PHONE, context.getString(R.string.screenshots_private_phone))
+                .putString(PREF_PRIVATE_WEBSITE, context.getString(R.string.screenshots_private_website))
+
+                .putString(PREF_COMPANY_NAME, context.getString(R.string.screenshots_company_name))
+                .putString(PREF_COMPANY_EMAIL, context.getString(R.string.screenshots_company_email))
+                .putString(PREF_COMPANY_ADDRESS, context.getString(R.string.screenshots_company_address))
+                .putString(PREF_COMPANY_TITLE, context.getString(R.string.screenshots_company_title))
+                .putString(PREF_COMPANY_COMPANY, context.getString(R.string.screenshots_company_company))
+                .putString(PREF_COMPANY_PHONE, context.getString(R.string.screenshots_company_phone))
+                .putString(PREF_COMPANY_WEBSITE, context.getString(R.string.screenshots_company_website))
+                .apply();
+    }
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction tabView = onView(withContentDescription("Company"));
+        tabView.perform(click());
+
+        ViewInteraction actionMenuItemView = onView(withId(R.id.action_settings));
+        actionMenuItemView.perform(click());
+    }
+}
