f542a7b38f6e58911bd10ac66989e48973375556
characterdog <43253617+characterdog@users.noreply.github.com>
2018-11-18 13:03:34 +0100

    Optimize imports

--- app/src/test/java/com/github/characterdog/share_my_number/ExampleUnitTest.java
+++ app/src/test/java/com/github/characterdog/share_my_number/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.github.characterdog.share_my_number;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
